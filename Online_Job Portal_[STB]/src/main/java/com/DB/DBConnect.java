196033a6d363df2b1bc1a564419f452dc152f271
Girija Prasada Mallik <116545197+GirijaprasadaMallik@users.noreply.github.com>
2024-10-22 13:41:10 +0530

    Update DBConnect.java

--- Online_Job Portal_[STB]/src/main/java/com/DB/DBConnect.java
+++ Online_Job Portal_[STB]/src/main/java/com/DB/DBConnect.java
@@ -5,23 +5,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnect {
-    
-    private static Connection Conn;
+	
+	private static Connection Conn;
 
-    public static Connection getconn() {
-        
-        try {
-            if(Conn==null) {
-                 Class.forName("com.mysql.cj.jdbc.Driver");
-                Conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/job_portal?autoReconnect=true&useSSL=false", "root", "password");
-            }
-            
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        
-        
-        return Conn;
-    }
+	public static Connection getconn() {
+		
+		try {
+			if(Conn==null) {
+				 Class.forName("com.mysql.cj.jdbc.Driver");
+				Conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/job_portal?autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true", "root", "264351");
+			}
+			
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		
+		return Conn;
+	}
 }
-
